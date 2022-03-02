60ec3ad2908f7fe488deae423020e4c7dc3c3f30
David Rogers <dhr@lanl.gov>
2022-03-02 16:06:15 -0500

    updating to paraview 5.10

--- postprocess/post_submit.sh
+++ postprocess/post_submit.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-#BSUB -P <compute_allocation> 
+#BSUB -P <compute_allocation>
 #BSUB -W 01:00
 #BSUB -nnodes 1
 #BSUB -J <pantheon_post_jid> 
 
-module load paraview
+pushd /sw/summit/paraview/spack-5.10.0 > /dev/null 2>&1
+source share/spack/setup-env.sh
+module load gcc/9.3.0
+spack compiler add
+spack load python;spack load py-numpy;spack load py-mpi4py;spack load py-pandas;spack load paraview
+spack unload openssl
+popd > /dev/null 2>&1
 
 SCRIPT=<pantheon_run_dir>/tubesSummitCdb.py
 
